e5f920f2dbe05a0b1a2cc9293242089e8e561a09
yanachepyk <yanapgp@gmail.com>
2023-03-27 21:12:44 +0300

    add local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,82 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Section from './Section/Section';
+import { nanoid } from 'nanoid';
+import Filter from './Filter/Filter';
+import { Container } from './Shared.styled';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem('contact'));
+    if (contacts) {
+      this.setState({ contacts });
+    }
+  }
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    const { name, number } = event.target.elements;
+    const isExist = this.state.contacts.some(
+      contact => contact.name === name.value
+    );
+
+    if (isExist) {
+      alert(`${name.value} is already in contacts.`);
+    } else {
+      this.setState(
+        prevState => {
+          return {
+            ...prevState,
+            contacts: [
+              ...prevState.contacts,
+              { name: name.value, id: nanoid(), number: number.value },
+            ],
+          };
+        },
+        () => {
+          localStorage.setItem('contact', JSON.stringify(this.state.contacts));
+          event.target.reset();
+        }
+      );
+    }
+  };
+
+  handleFilter = event => {
+    this.setState({ filter: event.target.value });
+  };
+
+  handleDelete = id => {
+    this.setState(prevState => ({
+      ...prevState,
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+    let contacts = JSON.parse(localStorage.getItem('contact'));
+    contacts = contacts.filter(contact => contact.id !== id);
+    localStorage.setItem('contact', JSON.stringify(contacts));
+  };
+
+  render() {
+    const filteredContacts = this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
+    );
+    return (
+      <Container>
+        <Section title="Phonebook">
+          <ContactForm onSubmit={this.handleFormSubmit} />
+        </Section>
+        <Section title="Contacts">
+          <Filter onFilter={this.handleFilter} />
+          <ContactList
+            contacts={filteredContacts}
+            onDelete={this.handleDelete}
+          />
+        </Section>
+      </Container>
+    );
+  }
+}
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+import { Button, Form, Input, Label } from '../Shared.styled';
+
+const ContactForm = ({ onSubmit }) => {
+  return (
+    <Form onSubmit={onSubmit}>
+      <Label>
+        Name
+        <Input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </Label>
+      <Label>
+        Number
+        <Input
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+      </Label>
+      <Button type="submit">Add contact</Button>
+    </Form>
+  );
+};
+
+ContactForm.propTypes = {
+  onSubmit: PropTypes.func,
+};
+
+export default ContactForm;
